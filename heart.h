4363b57f2873df2f15e9816f36530035ff505137
toha993 <43566487+toha993@users.noreply.github.com>
2019-01-23 01:23:26 +0600

    Add files via upload

--- heart.h
+++ heart.h
@@ -5,43 +5,98 @@
 #include <iostream>
 using namespace sf;
 using namespace std;
-class heart{
-public:
-    Texture CharacterTexture;
-    Sprite CharacterSprite;
-    float scaleSize = .015;
-	char loc[14];
-	//bool direction=true;
 
 
-    heart () {
-        CharacterTexture.loadFromFile("hrt (5).png");
-        CharacterSprite.setTexture(CharacterTexture);
-        CharacterSprite.scale(scaleSize, scaleSize);
-        CharacterSprite.setPosition(1300, 480);
-        CharacterSprite.setOrigin(CharacterSprite.getLocalBounds().width/2, CharacterSprite.getLocalBounds().height/2);
-//        CharacterSprite.scale(-1,1);
-    }
-    void Idle() {
-		static int x = 1;
-		sprintf(loc, "hrt (%d).png", x);
-		this->CharacterTexture.loadFromFile(loc);
-		this->CharacterSprite.setTexture(CharacterTexture);
-		if (x < 6) x++;
-		else x = 1;
-	}
-	float get()
-    {
-        return CharacterSprite.getPosition().x;
-    }
-    void udhao()
-    {
-        CharacterSprite.setPosition(-300, 480);
-    }
-    void DrawCharacter(RenderWindow &app) {
-            app.draw(CharacterSprite);
-        }
+class life
+{
+   Texture heart;
+   int curr=0,j=0,n=+5;
+   public:
+   Sprite hea[10];
+   int start_point=7;
+   bool f=0;
+   void init()
+   {
+       heart.loadFromFile("heart.png");
+       heart.setSmooth(true);
+       int i=0;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(0,0,252,217.5));
+       hea[0].setOrigin(127.5,108);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(276,0,492-276,217.5));
+       hea[1].setOrigin(396-276,108);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(516,0,694.5-516,217.5));
+       hea[2].setOrigin(628.5-516,108);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(696.5,0,836.5-696.5,217.5));
+       hea[3].setOrigin(790.5-696.5,108);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(862,0,938-862,217.5));
+       hea[4].setOrigin(902-862,108);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(82.5,217.5,168-82.5,239.5));
+       hea[5].setOrigin(124.5-82.5,101);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(192,217.5,334.4-192,239.5));
+       hea[6].setOrigin(238.5-192,101);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(333,217.5,516-333,239.5));
+       hea[7].setOrigin(396-333,101);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(516,217.5,763.5-516,239.5));
+       hea[8].setOrigin(630-516,101);
+       hea[i].scale(0.2,0.2);
+       i++;
+       hea[i].setPosition(start_point,530);
+       hea[i].setTexture(heart);
+       hea[i].setTextureRect(IntRect(763.5,217.5,1014-763.5,239.5));
+       hea[9].setOrigin(897-763.5,101);
+       hea[i].scale(0.2,0.2);
+   }
+   void movee()
+   {
+       j++;
+       if(j==n-1)curr++;
+       j%=n;
+       curr%=10;
+       // cout<<flag<<' '<<pa.getPosition().y<<' '<<curr_v<<endl;
+
+   }
+
+   void draw(RenderWindow &app)
+   {
+       app.draw(hea[curr]);
+   }
 };
+
+
 
 
 #endif // HEART_H_INCLUDED
